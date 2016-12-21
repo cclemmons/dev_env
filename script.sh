8261c5eaa6237e6a73e80b356def8496352d7388
Colin Clemmons <colin1006@gmail.com>
2016-12-20 23:03:22 -0600

    Adds .gitignore, init script, and vimrc

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Making Directories";
+mkdir -p ~/.vim/bundle ~/.vim/autoload;
+echo "Dowloaidng pathogen";
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
+echo "Downloading Solarized";
+git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized;
+echo "Downloading Vim Sensible";
+git clone git://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible;
+echo "Downloading Vim Surround";
+git clone git://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround;
+echo "Linking vimrc"
+ln -s ~/.vim/vimrc ~/.vimrc;
---
+++ vimrc
@@ -0,0 +1,24 @@
+execute pathogen#infect()
+inoremap jk <ESC>
+let mapleader = "\<Space>"
+filetype plugin indent on
+syntax on
+set encoding=utf-8
+set ruler
+set t_Co=256
+set nobackup
+set nowb
+set noswapfile
+set expandtab
+set smarttab
+set shiftwidth=4
+set laststatus=2
+set tabstop=4
+set shell=/bin/bash
+set nocompatible
+set number
+set colorcolumn=79
+
+colorscheme solarized
+set background=dark
+
